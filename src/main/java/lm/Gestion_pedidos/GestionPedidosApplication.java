bd34448e5ae023966bc1d1265693e109d87ae7d6
Lucas-nmp <lucas.morandeira.parejo@gmail.com>
2024-07-23 01:03:53 +0200

    Añadir controlador y vista principal para la integración del proyecto con Swing

--- src/main/java/lm/Gestion_pedidos/GestionPedidosApplication.java
+++ src/main/java/lm/Gestion_pedidos/GestionPedidosApplication.java
@@ -1,13 +1,26 @@
 package lm.Gestion_pedidos;
 
+import java.awt.EventQueue;
+import lm.Gestion_pedidos.controller.Controller;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GestionPedidosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionPedidosApplication.class, args);
+            ConfigurableApplicationContext contextSpring = new SpringApplicationBuilder(GestionPedidosApplication.class)
+                    .headless(false)
+                    .web(WebApplicationType.NONE)
+                    .run(args);
+            
+            EventQueue.invokeLater(() -> {
+                Controller controller = contextSpring.getBean(Controller.class);
+                controller.viewHomePage();
+            });
 	}
 
 }
--- src/main/java/lm/Gestion_pedidos/controller/Controller.java
+++ src/main/java/lm/Gestion_pedidos/controller/Controller.java
@@ -1,9 +1,40 @@
 package lm.Gestion_pedidos.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import lm.Gestion_pedidos.view.Homepage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
 /**
  *
  * @author Lucas
  */
-public class Controller {
+@Component
+public class Controller implements ActionListener{
+    
+    @Autowired
+    private ApplicationContext context;
+    
+    private Homepage homepage;
+    
+
+    public void viewHomePage() {
+        
+        homepage.setLocationRelativeTo(null);
+        homepage.setResizable(false);
+        homepage.setVisible(true); 
+    }
+    
+    @Autowired
+    public void setHomepage(Homepage homepage) {
+        this.homepage = homepage;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
     
 }
